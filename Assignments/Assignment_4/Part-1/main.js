c4ef0e23ea055a1a841b5eb61469cfe8eb878247
cody-roberts77 <cody.roberts@dcmail.ca>
2024-08-03 14:10:06 -0500

    First commit for Assignment 4 Part 2 - index.html, style.css, main.js and images folder added to working directory

--- Assignments/Assignment_4/Part-1/main.js
+++ Assignments/Assignment_4/Part-1/main.js
@@ -4,10 +4,11 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-function randomValueFromArray(array) {
-    const random = Math.floor(Math.random() * array.length);
-    return array[random];
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
 }
+
 
 
 let storyText = "It was 94 fahrenheit outside, so :insertX: went for a walk. When they got to :insertY:, they stared in horror for a few moments, then :insertZ:. Bob saw the whole thing, but was not surprised — :insertX: weighs 300 pounds, and it was a hot day.";
@@ -34,19 +35,19 @@ function result() {
     newStory = newStory.replace(":insertZ:", itemZ);
 
 
-    if (customName.value !== '') {
-        const name = customName.value;
-        newStory = newStory.replace("Bob", name);
+  if(customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replace("Bob", name);
 
-    }
+  }
 
-    if (document.getElementById("uk").checked) {
-        const weight = Math.round.concat(300 / 14, " Stone");
-        newStory = newStory.replace("300 pounds", weight);
-        const temperature = Math.round.concat((((94 - 32) * 5) / 9), " Centigrade");
-        newStory = newStory.replace("94 fahrenheit", temperature);
-    }
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round.concat(300 / 14, " Stone");
+    newStory = newStory.replace("300 pounds", weight);
+    const temperature =  Math.round.concat((((94 - 32)* 5) / 9), " Centigrade");
+    newStory = newStory.replace("94 fahrenheit", temperature);
+  }
 
-    story.textContent = newStory;
-    story.style.visibility = 'visible';
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
 }
