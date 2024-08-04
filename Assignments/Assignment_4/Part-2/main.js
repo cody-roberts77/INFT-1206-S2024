00016e6e003f5ca1bdb82a4591af55dd383a7afe
cody-roberts77 <cody.roberts@dcmail.ca>
2024-08-03 21:15:15 -0500

    Third commit for Assignment 4 Part 1 - Both arrays created, one for pics and one for description, for loop to loop through pics, attributes for newImage variable set for src and alt pointing to correct places

--- Assignments/Assignment_4/Part-2/main.js
+++ Assignments/Assignment_4/Part-2/main.js
@@ -6,13 +6,20 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const pics = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+
 /* Declaring the alternative text for each image file */
+
+const name = {'pic1.jpg': 'eye', 'pic2.jpg': 'swirl', 'pic3.jpg': 'flower', 'pic4.jpg': 'thoth', 'pic5.jpg': 'moth'}
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for(const image in pics){
 
+const newImage = document.createElement('img');
+
+newImage.setAttribute('src', 'images/${image}');
+newImage.setAttribute('alt', name[image]);
+thumbBar.appendChild(newImage);
+}
 /* Wiring up the Darken/Lighten button */
