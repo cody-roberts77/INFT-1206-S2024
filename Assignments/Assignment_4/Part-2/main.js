1b7bb8d712e450a06bdc1e2e6bd492844cec4530
cody-roberts77 <cody.roberts@dcmail.ca>
2024-08-04 14:34:26 -0500

    Fourth commit for Assignment 4 Part 2 - I Believe everything is working the way is suppossed too, I played around with the use of for in loop but I think the regular for loop works better, I really struggled with getting the images to actually display and I think that was because of the way I was declaring them in the array. Going to go over everything quickly before final commit!

--- Assignments/Assignment_4/Part-2/main.js
+++ Assignments/Assignment_4/Part-2/main.js
@@ -5,21 +5,42 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const pics = ["images/pic1.jpg", "images/pic2.jpg", "images/pic3.jpg", "images/pic4.jpg", "images/pic5.jpg"];
 
-const pics = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+let newImage = new Image();
+
+
 
 /* Declaring the alternative text for each image file */
 
-const name = {'pic1.jpg': 'eye', 'pic2.jpg': 'swirl', 'pic3.jpg': 'flower', 'pic4.jpg': 'thoth', 'pic5.jpg': 'moth'}
+const name = ["Eye", "Swirl", "Flower", "Thoth", "Moth"]
 
 /* Looping through images */
 
-for(const image in pics){
+for(let i = 0; i < pics.length; i++){
 
-const newImage = document.createElement('img');
+const img = document.createElement('img');
 
-newImage.setAttribute('src', 'images/${image}');
-newImage.setAttribute('alt', name[image]);
-thumbBar.appendChild(newImage);
+img.src = pics[i];
+img.alt = name[i];
+thumbBar.appendChild(img);
+img.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
 }
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  });
+  
